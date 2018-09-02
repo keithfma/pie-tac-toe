1e4274dd3aa4182b39f81ce3c1e047c3befec4c3
Keith Ma <keith@telluslabs.com>
2018-09-02 01:46:04 -0400

    Build table function

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ var players = [
 ];
 var board_width = 3;
 var board_height = 3;
+var board = [];
 var curr_players = {
     x: null,
     o: null
@@ -22,8 +23,34 @@ function update_player(event) {
     // update player image
     img_elem = document.getElementById('player-image-' + marker);
     img_elem.src = 'icons/' + [menu_elem.value] + '.jpg';
-    // DEBUG
-    console.log(marker, curr_players[marker], img_elem.src);
+}
+
+// create (blank) game board of specified size
+function create_game_board() {
+    board_div = document.getElementById('game-board-div');
+    // TODO: update dimensions from menus
+    // remove existing board
+    if (document.contains(document.getElementById('game-board-table'))) {
+        document.getElementById('game-board-element').remove();
+        board = [];
+    }
+    // create new board
+    var board_tbl = document.createElement('table');
+    board_tbl.id = 'game-board-table';
+    for (let ii = 0; ii < board_height; ii++) {
+        board.push([]);
+        var row = document.createElement('tr');
+        for (let jj = 0; jj < board_width; jj++) {
+            board[ii].push(null);
+            var col = document.createElement('td');
+            col.id = ii.toString() + '-' + jj.toString();
+            row.appendChild(col);
+        }
+        board_tbl.appendChild(row);
+    }
+    board_div.appendChild(board_tbl);
+    // DEBUG 
+    console.log(board);
 }
 
 // init function, runs automatically on page load
@@ -46,7 +73,11 @@ document.addEventListener('DOMContentLoaded', function() {
         rand_player = players[Math.floor(Math.random() * players.length)];
         menu.value = rand_player;
         menu.dispatchEvent(new Event('change'));
-   
     }
+
+    // initialize game board, assign listeners
+    create_game_board();
+    // TODO: listener for new game
+    // TODO: listeners for changed board size
 
 }, false);
