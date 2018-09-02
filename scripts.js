02b46eae1f8092778c940defe2b17fcc760a289b
Keith Ma <keith@telluslabs.com>
2018-09-02 01:14:10 -0400

    Working user selection including avatars

--- scripts.js
+++ scripts.js
@@ -1,19 +1,29 @@
 // globals
 var markers = ['x', 'o'];
 var players = [
-    {name: "Adam", img: null},
-    {name: "Erik", img: null},
-    {name: "Keith", img: null},
-    {name: "Odin", img: null}
+    "Adam", 
+    "Erik", 
+    "Keith",
+    "Odin" 
 ];
 var board_width = 3;
 var board_height = 3;
+var curr_players = {
+    x: null,
+    o: null
+};
 
 // update board to reflect selected players
-function update_players() {
-    for (let marker of markers) {
-        console.log("Update player " + marker);
-    }
+function update_player(event) {
+    // update curr_players global
+    menu_elem = event.target;
+    marker = menu_elem.id.slice(-1);
+    curr_players[marker] = menu_elem.value;
+    // update player image
+    img_elem = document.getElementById('player-image-' + marker);
+    img_elem.src = 'icons/' + [menu_elem.value] + '.jpg';
+    // DEBUG
+    console.log(marker, curr_players[marker], img_elem.src);
 }
 
 // init function, runs automatically on page load
@@ -24,12 +34,19 @@ document.addEventListener('DOMContentLoaded', function() {
         choose_player = document.getElementById("choose-player-" + marker);
             for (let player of players) {
                 var option = document.createElement('option');
-                option.text = player.name;
+                option.text = player;
                 choose_player.add(option);
             } 
     }
 
-    // TODO: randomize initial player selection
-    update_players();
+    // player selection: initialize and assign listeners
+    for (let marker of markers) {
+        var menu = document.getElementById("choose-player-" + marker);
+        menu.addEventListener('change', update_player);
+        rand_player = players[Math.floor(Math.random() * players.length)];
+        menu.value = rand_player;
+        menu.dispatchEvent(new Event('change'));
+   
+    }
 
 }, false);
